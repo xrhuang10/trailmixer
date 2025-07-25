edc6c5d972838d32934bc11fe06f630a2e402393
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-07-24 21:39:32 -0400

    add VideoUploadRequest + VideoStitchRequest

--- app/models.py
+++ app/models.py
@@ -1,10 +1,18 @@
 from pydantic import BaseModel
+from typing import List
+from fastapi import UploadFile, File
+
+class VideoUploadRequest(BaseModel):
+    files: List[UploadFile] = File(...)
 
 class VideoUploadResponse(BaseModel):
     message: str
     status: str
     filenames: list[str]
     
+class VideoStitchRequest(BaseModel):
+    filenames: List[str]
+
 class VideoStitchResponse(BaseModel):
     message: str
     status: str
